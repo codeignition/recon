3ad777a72e2566d3c7838dd3960a37c505585754
Hari haran <hariharan.uno@gmail.com>
2015-07-06 14:23:17 +0530

    add internal package fileutil

---
+++ internal/fileutil/fileutil.go
@@ -0,0 +1,18 @@
+// Copyright 2015 Hari haran. All rights reserved.
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file.
+
+// fileutil provides utility functions for dealing with files.
+package fileutil
+
+import (
+	"os"
+)
+
+// Exists returns true if a file exists.
+func Exists(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		return true
+	}
+	return false
+}
