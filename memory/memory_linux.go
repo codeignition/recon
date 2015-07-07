8d34dfd5e686b463cf869d73c3a6325e19b1bd57
Hari haran <hariharan.uno@gmail.com>
2015-07-07 17:34:32 +0530

    Add length check for slice to avoid runtime panic

--- memory/memory_linux.go
+++ memory/memory_linux.go
@@ -32,58 +32,60 @@ func CollectData() (Data, error) {
 	s := bufio.NewScanner(f)
 	for s.Scan() {
 		l := strings.Split(s.Text(), ":")
-		k, v := l[0], strings.TrimSpace(l[1])
-		switch k {
-		case "MemTotal":
-			d["total"] = v
-		case "MemFree":
-			d["free"] = v
-		case "MemAvailable":
-			d["available"] = v
-		case "Buffers":
-			d["buffers"] = v
-		case "Cached":
-			d["cached"] = v
-		case "SwapCached":
-			swap["cached"] = v
-		case "Active":
-			d["active"] = v
-		case "Inactive":
-			d["inactive"] = v
-		case "SwapTotal":
-			swap["total"] = v
-		case "SwapFree":
-			swap["free"] = v
-		case "Dirty":
-			d["dirty"] = v
-		case "Writeback":
-			d["writeback"] = v
-		case "AnonPages":
-			d["anon_pages"] = v
-		case "Mapped":
-			d["mapped"] = v
-		case "Slab":
-			d["slab"] = v
-		case "SReclaimable":
-			d["slab_reclaimable"] = v
-		case "SUnreclaim":
-			d["slab_unreclaim"] = v
-		case "PageTables":
-			d["page_tables"] = v
-		case "NFS_Unstable":
-			d["nfs_unstable"] = v
-		case "Bounce":
-			d["bounce"] = v
-		case "CommitLimit":
-			d["commit_limit"] = v
-		case "Committed_AS":
-			d["committed_as"] = v
-		case "VmallocTotal":
-			d["vmalloc_total"] = v
-		case "VmallocUsed":
-			d["vmalloc_used"] = v
-		case "VmallocChunk":
-			d["vmalloc_chunk"] = v
+		if len(l) == 2 {
+			k, v := l[0], strings.TrimSpace(l[1])
+			switch k {
+			case "MemTotal":
+				d["total"] = v
+			case "MemFree":
+				d["free"] = v
+			case "MemAvailable":
+				d["available"] = v
+			case "Buffers":
+				d["buffers"] = v
+			case "Cached":
+				d["cached"] = v
+			case "SwapCached":
+				swap["cached"] = v
+			case "Active":
+				d["active"] = v
+			case "Inactive":
+				d["inactive"] = v
+			case "SwapTotal":
+				swap["total"] = v
+			case "SwapFree":
+				swap["free"] = v
+			case "Dirty":
+				d["dirty"] = v
+			case "Writeback":
+				d["writeback"] = v
+			case "AnonPages":
+				d["anon_pages"] = v
+			case "Mapped":
+				d["mapped"] = v
+			case "Slab":
+				d["slab"] = v
+			case "SReclaimable":
+				d["slab_reclaimable"] = v
+			case "SUnreclaim":
+				d["slab_unreclaim"] = v
+			case "PageTables":
+				d["page_tables"] = v
+			case "NFS_Unstable":
+				d["nfs_unstable"] = v
+			case "Bounce":
+				d["bounce"] = v
+			case "CommitLimit":
+				d["commit_limit"] = v
+			case "Committed_AS":
+				d["committed_as"] = v
+			case "VmallocTotal":
+				d["vmalloc_total"] = v
+			case "VmallocUsed":
+				d["vmalloc_used"] = v
+			case "VmallocChunk":
+				d["vmalloc_chunk"] = v
+			}
 		}
 	}
 	if err := s.Err(); err != nil {
