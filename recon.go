3ca7157bc4fc655a25629d637bb7836d8b1b66aa
Hari haran <hariharan.uno@gmail.com>
2015-07-07 11:37:24 +0530

    Add cpudata in main

--- recon.go
+++ recon.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/hariharan-uno/recon/cpu"
 	"github.com/hariharan-uno/recon/lsb"
 	"github.com/hariharan-uno/recon/memory"
 )
@@ -24,13 +25,19 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+	cpudata, err := cpu.CollectData()
+	if err != nil {
+		log.Println(err)
+	}
 
 	data := struct {
 		Lsb    lsb.Data    `json:"lsb"`
 		Memory memory.Data `json:"memory"`
+		Cpu    cpu.Data    `json:"cpu"`
 	}{
 		lsbdata,
 		memdata,
+		cpudata,
 	}
 	b, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
