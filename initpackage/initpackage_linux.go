a71f748c7970c3d24678b31fab3083390e4ee333
Hari haran <hariharan.uno@gmail.com>
2015-07-09 11:29:13 +0530

    Add initpackage
    Still some doubts on the package API. Refactor later if necessary.

---
+++ initpackage/initpackage_linux.go
@@ -0,0 +1,24 @@
+// Copyright 2015 Hari haran. All rights reserved.
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file.
+
+// Package initpackage exposes a single variable which
+// says the init package the system is using, e.g. Systemd
+package initpackage
+
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+// Name is the name of the init package used by the system
+var Name string
+
+func init() {
+	if b, err := ioutil.ReadFile("/proc/1/comm"); err == nil {
+		Name = strings.TrimSpace(string(b))
+	} else {
+		log.Println("initpackage: ", err)
+	}
+}
