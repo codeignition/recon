7d75cc18ce6dd14efdfffcfd47c6356a2130f84d
Hari haran <hariharan.uno@gmail.com>
2015-07-09 16:22:45 +0530

    Add ps package

---
+++ ps/ps.go
@@ -0,0 +1,9 @@
+// Copyright 2015 Hari haran. All rights reserved.
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file.
+
+// +build darwin linux solaris
+
+package ps
+
+var Command = "ps -ef"
