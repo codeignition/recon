87fa5da541f9e184c446025b994002eb11ed07ba
Hari haran <hariharan.uno@gmail.com>
2015-08-07 11:18:07 +0530

    add Agent type in package recon
    The master server (marksman) and recond both have the Agent type,
    so it makes sense to have a common type under the package recon.

---
+++ agent.go
@@ -0,0 +1,11 @@
+// Copyright 2015 CodeIgnition. All rights reserved.
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file.
+
+package recon
+
+// Agent represents a recon daemon running on
+// a machine.
+type Agent struct {
+	UID string
+}
