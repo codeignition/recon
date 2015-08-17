44f9fb55a45d92f13525cf3983df6980b7418b71
Hari haran <hariharan.uno@gmail.com>
2015-08-17 12:20:01 +0530

    add more tests for config

--- cmd/recond/config/config_test.go
+++ cmd/recond/config/config_test.go
@@ -6,8 +6,13 @@ package config
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/codeignition/recon/internal/fileutil"
 )
 
 func TestGenerateUID(t *testing.T) {
@@ -34,6 +39,95 @@ func TestParseConfig(t *testing.T) {
 		t.Error(err)
 	}
 	if c.UID != fakeUID {
-		t.Errorf("got %s; want %s", c.UID, fakeUID)
+		t.Errorf("got config UID %q; want %q", c.UID, fakeUID)
+	}
+}
+
+func TestInitExisting(t *testing.T) {
+	f, err := ioutil.TempFile("", "recond_fake_config")
+	if err != nil {
+		t.Error(err)
+	}
+	fakeUID := "13fcdf794886"
+	fakeContent := fmt.Sprintf(`{"uid":"%s"}
+`, fakeUID)
+	_, err = f.WriteString(fakeContent)
+	if err != nil {
+		t.Error(err)
+	}
+	if err := f.Close(); err != nil {
+		t.Error(err)
+	}
+	configPath = f.Name()
+	c, err := Init()
+	if err != nil {
+		t.Error(err)
+	}
+	if c.UID != fakeUID {
+		t.Errorf("got config UID %q; want %q", c.UID, fakeUID)
+	}
+	if err := os.Remove(f.Name()); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInitNew(t *testing.T) {
+	configPath = filepath.Join(os.TempDir(), "recond_fake_new_config") // doesn't exist
+	c, err := Init()
+	if err != nil {
+		t.Error(err)
+	}
+	if !fileutil.Exists(configPath) {
+		t.Errorf("Init didn't create the config file")
+	}
+	if c.UID == "" {
+		t.Errorf("got config UID as an empty string; want a non empty string")
+	}
+	if err := os.Remove(configPath); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSave(t *testing.T) {
+	f, err := ioutil.TempFile("", "recond_fake_config")
+	if err != nil {
+		t.Error(err)
+	}
+	fakeUID := "13fcdf794886"
+	fakeContent := fmt.Sprintf(`{"uid":"%s"}
+`, fakeUID)
+	_, err = f.WriteString(fakeContent)
+	if err != nil {
+		t.Error(err)
+	}
+	if err := f.Close(); err != nil {
+		t.Error(err)
+	}
+	configPath = f.Name()
+	c, err := Init()
+	if err != nil {
+		t.Error(err)
+	}
+	// this is redundant as it is checked in TestInit but lets leave it anyways
+	if c.UID != fakeUID {
+		t.Errorf("got config UID %q; want %q", c.UID, fakeUID)
+	}
+	s, err := generateUID()
+	if err != nil {
+		t.Error(err)
+	}
+	c.UID = s
+	if err := c.Save(); err != nil {
+		t.Error(err)
+	}
+	c, err = Init()
+	if err != nil {
+		t.Error(err)
+	}
+	if c.UID != s {
+		t.Errorf("got config UID %q; want %q", c.UID, s)
+	}
+	if err := os.Remove(f.Name()); err != nil {
+		t.Error(err)
 	}
 }
