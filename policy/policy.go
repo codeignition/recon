56f7d5c0f888bbd1c8e28609561bfa14fedc7d51
Hari haran <hariharan.uno@gmail.com>
2015-08-31 15:01:32 +0530

    policy: fix comments and naming

--- policy/policy.go
+++ policy/policy.go
@@ -10,30 +10,29 @@ import (
 	"golang.org/x/net/context"
 )
 
-// Policy is the map containing the rules of a particular monitoring policy.
-//
-// e.g. "tcp" PolicyType requires 2 policy keys "port" and "frequency"
+// Policy represents a monitoring policy
 type Policy struct {
-	Name     string
-	AgentUID string
-	Type     string // Type denotes the monitoring policy type. e.g. "tcp"
-	M        map[string]string
+	Name     string            // Name of the monitoring policy
+	AgentUID string            // Agent UID
+	Type     string            // Type denotes the monitoring policy type. e.g. "tcp"
+	M        map[string]string // M is the map containing the rules of a particular monitoring policy.
 }
 
-// Config is the format used to encode/decode the monitoring policy
-// received from the message queue or to store in the config file
+// Config is a slice of monitoring policies
 type Config []Policy
 
+// HandlerFunc is the type of a policy handler function. Any policy handler
+// function must be of this type.
 type HandlerFunc func(context.Context, Policy) (<-chan Event, error)
 
-// policyFuncMap maps a policy type to a handler function
-var policyFuncMap = make(map[string]HandlerFunc)
+// handlerFuncMap maps a policy type to a handler function
+var handlerFuncMap = make(map[string]HandlerFunc)
 
 func (p Policy) Execute(ctx context.Context) (<-chan Event, error) {
 	if err := p.Valid(); err != nil {
 		return nil, err
 	}
-	return policyFuncMap[p.Type](ctx, p)
+	return handlerFuncMap[p.Type](ctx, p)
 }
 
 // Valid checks whether the policy is valid.
@@ -41,7 +40,7 @@ func (p Policy) Valid() error {
 	if p.Name == "" {
 		return errors.New("policy name can't be empty")
 	}
-	if _, ok := policyFuncMap[p.Type]; !ok {
+	if _, ok := handlerFuncMap[p.Type]; !ok {
 		return errors.New("policy type unknown")
 	}
 	return nil
@@ -52,9 +51,9 @@ func RegisterHandler(policyType string, handlerFunc HandlerFunc) error {
 		return errors.New("policy type can't be empty")
 	}
 
-	if _, ok := policyFuncMap[policyType]; ok {
+	if _, ok := handlerFuncMap[policyType]; ok {
 		return errors.New("handler for the policy type already exists")
 	}
-	policyFuncMap[policyType] = handlerFunc
+	handlerFuncMap[policyType] = handlerFunc
 	return nil
 }
