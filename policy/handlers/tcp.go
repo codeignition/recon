66bd4a94a8961b19c5450e27214aec1781c1ec61
Hari haran <hariharan.uno@gmail.com>
2015-08-31 14:53:25 +0530

    change tcp policy keys and use ctx to close events channel

--- policy/handlers/tcp.go
+++ policy/handlers/tcp.go
@@ -16,13 +16,13 @@ import (
 
 func TCP(ctx context.Context, p policy.Policy) (<-chan policy.Event, error) {
 	// Always use v, ok := p[key] form to avoid panic
-	port, ok := p.M["port"]
+	addr, ok := p.M["address"]
 	if !ok {
-		return nil, errors.New(`"port" key missing in tcp policy`)
+		return nil, errors.New(`"address" key missing in tcp policy`)
 	}
-	freq, ok := p.M["frequency"]
+	interval, ok := p.M["interval"]
 	if !ok {
-		return nil, errors.New(`"frequency" key missing in tcp policy`)
+		return nil, errors.New(`"interval" key missing in tcp policy`)
 	}
 
 	// From the time package docs:
@@ -32,39 +32,46 @@ func TCP(ctx context.Context, p policy.Policy) (<-chan policy.Event, error) {
 	// decimal numbers, each with optional fraction and a unit suffix,
 	// such as "300ms", "-1.5h" or "2h45m".
 	// Valid time units are "ns", "us", "ms", "s", "m", "h".
-	d, err := time.ParseDuration(freq)
+	d, err := time.ParseDuration(interval)
 	if err != nil {
 		return nil, err
 	}
 
 	// This check is here to ensure time.Ticker(d) doesn't panic
 	if d <= 0 {
-		return nil, errors.New("frequency must be a positive quantity")
+		return nil, errors.New("interval must be a positive quantity")
 	}
 
 	out := make(chan policy.Event)
 	go func() {
-		for now := range time.Tick(d) {
-			_, err := net.DialTimeout("tcp", port, d)
-			if err != nil {
-				out <- policy.Event{
-					Time:   now,
-					Policy: p,
-					Data: map[string]interface{}{
-						"status": "failure",
-						"error":  err,
-					},
-				}
-			} else {
-				out <- policy.Event{
-					Time:   now,
-					Policy: p,
-					Data: map[string]interface{}{
-						"status": "success",
-					},
+		t := time.NewTicker(d)
+		for {
+			select {
+			case <-ctx.Done():
+				t.Stop()
+				close(out)
+				return
+			case <-t.C:
+				_, err := net.DialTimeout("tcp", addr, d)
+				if err != nil {
+					out <- policy.Event{
+						Time:   time.Now(),
+						Policy: p,
+						Data: map[string]interface{}{
+							"status": "failure",
+							"error":  err.Error(),
+						},
+					}
+				} else {
+					out <- policy.Event{
+						Time:   time.Now(),
+						Policy: p,
+						Data: map[string]interface{}{
+							"status": "success",
+						},
+					}
 				}
 			}
-			// TODO: think about when to close the out channel .
 		}
 	}()
 	return out, nil
